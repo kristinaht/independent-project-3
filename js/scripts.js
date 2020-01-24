51e6fd232d6cf2823a56b72f9fab6f4a32b5eac0
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-01-24 09:09:53 -0800

    add user interface logic needed for testing

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("form#form").submit(function(event){
+    event.preventDefault();
+    var number = $("#number").val();
+    
+    for(i=0; i<=number; i++){
+      
+    }
+
+  });
+});
