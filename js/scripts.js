95e2ef30220060b4179cf503bc43fc82cff5f39c
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-01-24 10:21:23 -0800

    unsuccessfully test first spec in README

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,28 @@
 $(document).ready(function(){
   $("form#form").submit(function(event){
     event.preventDefault();
-    var number = $("#number").val();
+    var number = ($("#number").val());
+    var numArray = number.split("");
+    var exceptions= [1, 2, 3]; 
     
-    for(i=0; i<=number; i++){
-      
-    }
+    var returnResult=function(number){
+      for (var index = 1; index < number; index ++){
+        // return(index);
+        $(".result").text(returnResult);
+      }
+      }
 
+     
+      
   });
 });
+
+
+// function findExceptions(numArray,exceptions){
+//   for(var index = 0; index<numArray.length; index++){
+//     if(numArray.includes(exceptions[index])) {
+//       return index;
+//     }
+//   }
+// }
+
