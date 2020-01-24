6822cfa0d39b6b53ba64d62c6a3ca66620c16223
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-01-24 10:57:33 -0800

    attempt to fix logic so that it passes first spect testing

--- js/scripts.js
+++ js/scripts.js
@@ -4,19 +4,30 @@ $(document).ready(function(){
     var number = ($("#number").val());
     var numArray = number.split("");
     var exceptions= [1, 2, 3]; 
-    
-    var returnResult=function(number){
-      for (var index = 1; index < number; index ++){
-        // return(index);
-        $(".result").text(returnResult);
-      }
-      }
+    var result = beepBoopGame(number);
+    $(".result").text(result);
+  
+
+    // var result = numArray;
+    // $(".result").text(result);
+    // var returnResult=function(number){
+    //   for (var index = 1; index < number; index ++){
+    //     // return(index);
+    //     $(".result").text(returnResult);
+    //   }
+    //   }
 
      
       
   });
 });
-
+function beepBoopGame(number){  
+  numberRange = [];
+  for(index = 0; index <= number; index++){
+    numberRange.push(index);
+    return (numberRange);
+}
+}
 
 // function findExceptions(numArray,exceptions){
 //   for(var index = 0; index<numArray.length; index++){
