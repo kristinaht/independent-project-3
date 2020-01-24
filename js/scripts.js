fa15025bcd4c6a65f4f8c5c5b82fe9a760d69f1a
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-01-24 12:17:34 -0800

    test different logic related to first spec

--- js/scripts.js
+++ js/scripts.js
@@ -10,15 +10,42 @@ $(document).ready(function() {
   $("form#form").submit(function(event) {
     event.preventDefault();
     var numberInput =$("input#number").val();
-    console.log(numberInput);
-    var numberArr = numberInput.split("");
-    console.log(numberArr);
+    
+    // var numberArr = numberInput.split("");
+    
+
+    $("div.result").text(beepBoop(numberInput));
+    console.log(beepBoop(numberInput));
     //call beepBop() function on each word in the numberArr
     // for(var i = 0; i < numberArr.length; i++) {
     //   $(".output").append(" " + translate(wordArr[i]));
     // }
   });
 });
+
+
+function beepBoop(numberInput){
+  var numberRange=[];
+  for(var index=0; index<=numberInput;index++){
+    if(numberInput) {
+      numberRange.push(index);
+      return numberRange;
+    
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 // $(document).ready(function(){
 //   $("form#form").submit(function(event){
 //     event.preventDefault();
